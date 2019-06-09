899d4f443ba6ea599908932e6384f041ec7cd597
Алексей В <nrad2004@mail.ru>
2019-06-09 09:36:26 +0700

    up

---
+++ server.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile('index.html', { root: __dirname });
+});
+
+app.get('/build.js', (req, res) => {
+    res.sendFile('./dist/build.js', { root: __dirname });
+});
+
+let port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(__dirname);
+    console.log("Listening Port localhost:" + port);
+});
